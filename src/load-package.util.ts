fa98bffdc25e62c8cb82f2835186dab44daf4eec
Rodrigo <monstawoodwow@gmail.com>
2023-07-12 04:01:33 +0100

    refactor: lazy load graphql package being optional

---
+++ src/load-package.util.ts
@@ -0,0 +1,23 @@
+const MISSING_REQUIRED_DEPENDENCY = (name: string, reason: string) =>
+    `The "${name}" package is missing. Please, make sure to install this library ($ npm install ${name}) to take advantage of ${reason}.`;
+
+/**
+ * Lazy loads a package
+ * @see https://github.com/nestjs/nest/blob/master/packages/common/utils/load-package.util.ts#L8 (fork) 
+ * @param packageName 
+ * @param context 
+ * @param loaderFn 
+ * @returns 
+ */
+export function loadPackage(
+    packageName: string,
+    context: string,
+    loaderFn?: Function,
+) {
+    try {
+        return loaderFn ? loaderFn() : require(packageName);
+    } catch (e) {
+        console.error(MISSING_REQUIRED_DEPENDENCY(packageName, context));
+        process.exit(1);
+    }
+}
